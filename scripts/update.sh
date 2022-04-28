04a84b251ec98d0cd333e36dc5be22e99d387514
Ross Wickman <rosswickman@users.noreply.github.com>
2022-04-28 15:22:34 -0500

    Update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,8 +2,10 @@
 
 echo "Updating AWS CLI v2..."
 echo(
+
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip -q -o awscliv2.zip
 sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
+
 echo(
 echo "AWS CLI v2 updated!"
