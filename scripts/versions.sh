fe79b78d7c453a0ea048b1bafc0b57626e597a1d
Ross Wickman <rosswickman@users.noreply.github.com>
2022-04-28 15:33:36 -0500

    Update versions.sh

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -4,33 +4,26 @@ echo ""
 echo "Getting Shell Versions..."
 echo ""
 
-echo "Bash:"
 bash --version
 echo ""
 
-echo "Z Shell:"
 zsh --version
 echo ""
 
-echo "Powershell (pwsh):"
 pwsh --version
 echo ""
 
 echo "Getting AWS CLI Versions..."
 echo ""
 
-echo "AWS CLI:"
 aws --version
 echo ""
 
-echo "EB CLI:"
 eb --version
 echo ""
 
-echo "ECS CLI:"
 ecs-cli --version
 echo ""
 
-echo "SAM CLI:"
 sam --version
 echo ""
